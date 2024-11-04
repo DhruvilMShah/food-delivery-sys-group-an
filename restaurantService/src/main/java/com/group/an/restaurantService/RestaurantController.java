7613a596a9e7cd24b1e04cad3173e87c246dcf13
Dhruvil Shah <dhruvilkoup0704@gmail.com>
2024-11-04 10:44:35 +0530

    can update/delete menu items of a restaurant menu

--- restaurantService/src/main/java/com/group/an/restaurantService/RestaurantController.java
+++ restaurantService/src/main/java/com/group/an/restaurantService/RestaurantController.java
@@ -57,4 +57,66 @@ public class RestaurantController {
 			return ResponseEntity.notFound().build();
 		}
 	}
+
+	@PostMapping("/restaurants/{restaurantId}/menu/{menuItemId}")
+	public ResponseEntity<Restaurants> updateMenuItemOfRestaurant(@RequestBody MenuItem item,
+																  @PathVariable("restaurantId") Integer restaurantId,
+																  @PathVariable("menuItemId") Integer menuItemId) {
+		// ToDo: Validate if restaurantId on the endpoint and the restaurantId of the logged in RestaurantOwner is same
+		Optional<Restaurants> restaurant = restaurantRepository.findById(restaurantId);
+		if (restaurant.isPresent()) {
+			List<MenuItem> existingMenuItems = restaurant.get().getMenus();
+
+			if(existingMenuItems == null) {
+				existingMenuItems = new ArrayList<>();
+				existingMenuItems.add(item);
+			}
+			else {
+				existingMenuItems = replaceExistingItem(existingMenuItems, menuItemId,item);
+			}
+			restaurant.get().setMenus(existingMenuItems);
+			restaurantRepository.save(restaurant.get());
+			return ResponseEntity.ok(restaurant.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	private List<MenuItem> replaceExistingItem(List<MenuItem> existingMenuItems, Integer menuItemId, MenuItem item) {
+		for(MenuItem existingItem : existingMenuItems) {
+			if(existingItem.getMenuItemId() == menuItemId) {
+				existingMenuItems.remove(existingItem);
+				existingMenuItems.add(item);
+				break;
+			}
+		}
+		return existingMenuItems;
+	}
+
+	@DeleteMapping("/restaurants/{restaurantId}/menu/{menuItemId}")
+	public ResponseEntity<Restaurants> deleteMenuItemOfRestaurant(@PathVariable("restaurantId") Integer restaurantId,
+																  @PathVariable("menuItemId") Integer menuItemId) {
+		// ToDo: Validate if restaurantId on the endpoint and the restaurantId of the logged in RestaurantOwner is same
+		Optional<Restaurants> restaurant = restaurantRepository.findById(restaurantId);
+		if (restaurant.isPresent()) {
+			List<MenuItem> existingMenuItems = restaurant.get().getMenus();
+
+			if(existingMenuItems == null || existingMenuItems.isEmpty()) {
+				return ResponseEntity.notFound().build();
+			}
+			else {
+				for(MenuItem existingItem : existingMenuItems) {
+					if(existingItem.getMenuItemId() == menuItemId) {
+						existingMenuItems.remove(existingItem);
+						break;
+					}
+				}
+			}
+			restaurant.get().setMenus(existingMenuItems);
+			restaurantRepository.save(restaurant.get());
+			return ResponseEntity.ok(restaurant.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
