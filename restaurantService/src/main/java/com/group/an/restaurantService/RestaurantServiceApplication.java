6d4545c64235c4090e87bb4fb1a1b6a5c2651cd9
Dhruvil Shah <dhruvilkoup0704@gmail.com>
2024-11-09 17:24:09 +0530

    bringing up restaurant service

--- restaurantService/src/main/java/com/group/an/restaurantService/RestaurantServiceApplication.java
+++ restaurantService/src/main/java/com/group/an/restaurantService/RestaurantServiceApplication.java
@@ -1,13 +1,26 @@
 package com.group.an.restaurantService;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.group.an.dataService.repositories")
 public class RestaurantServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantServiceApplication.class, args);
+	}
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("OrderService API Documentation")
+						.version("1.0")
+						.description("APIs for Managing Orders in the Food Delivery System"));
 	}
 
 }
